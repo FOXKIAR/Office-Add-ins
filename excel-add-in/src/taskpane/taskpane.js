b54aa5979f876752d2466bb6a22c1276ba62e3a8
foxkiar <2399786141@qq.com>
2025-02-12 00:43:03 +0800

    update files

--- excel-add-in/src/taskpane/taskpane.js
+++ excel-add-in/src/taskpane/taskpane.js
@@ -10,6 +10,7 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+    document.getElementById("rand").onclick = random;
   }
 });
 
@@ -24,6 +25,24 @@ export async function run() {
       // Update the fill color.
       range.format.fill.color = "yellow";
 
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function random() {
+  try {
+    await Excel.run(async (context) => {
+      const range = context.workbook.getSelectedRange();
+      range.load("address");
+      const blue = Math.floor(Math.random() * 256);
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+
+      range.format.fill.color = red.toString(16) + green.toString(16) + blue.toString(16);
       await context.sync();
       console.log(`The range address was ${range.address}.`);
     });
