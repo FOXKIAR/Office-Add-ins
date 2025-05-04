e89dbd7922728ffdf86d4db0e2701861474c74ce
foxkiar <2399786141@qq.com>
2025-05-04 18:05:37 +0800

    Test with cmd

--- ppt-add-in/src/commands/commands.js
+++ ppt-add-in/src/commands/commands.js
@@ -7,7 +7,14 @@
 
 Office.onReady(() => {
   // If needed, Office.js is ready to be called.
+  if (info.host === Office.HostType.PowerPoint) {
+    document.getElementById("run").onclick = run;
+  }
 });
+
+function run(){
+  Office.context.document.setSelectedDataAsync("Hello World!");
+}
 
 /**
  * Shows a notification when the add-in command is executed.
