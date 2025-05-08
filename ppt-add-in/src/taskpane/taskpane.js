c3a4d9e2a0df437055f1cda7fd62f953a3bb29aa
beiminghaoze <beiminghaoze@outlook.com>
2025-05-08 13:10:38 +0800

    测试插入图片

--- ppt-add-in/src/taskpane/taskpane.js
+++ ppt-add-in/src/taskpane/taskpane.js
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
+import { logo } from "../../assets/logo-filled.png"
 
 /* global document, Office */
 
@@ -18,8 +19,22 @@ export async function run() {
   /**
    * Insert your PowerPoint code here
    */
-  const options = { coercionType: Office.CoercionType.Text };
+  const options = { coercionType: Office.CoercionType.Matrix };
 
   await Office.context.document.setSelectedDataAsync(" ", options);
   await Office.context.document.setSelectedDataAsync("Hello World!", options);
+}
+
+export async function test() {
+  /**
+   * Insert your PowerPoint code here
+   */
+  const options = { coercionType: Office.CoercionType.Image };
+
+  const reader = new FileReader();
+  reader.readAsDataURL(logo);
+
+  const logoBase64Str = reader.result.split(",")[1];
+
+  await Office.context.document.setSelectedDataAsync(logoBase64Str, options);
 }
